8a48f14c407b4508164ca636c4de47799dd9f19e
Guilherme Silva <guilherme.ssantos@bandtec.com.br>
2021-01-20 16:09:17 -0300

    New implementations added

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,6 +2,13 @@ object Dependencies {
 
     const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
     const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
-    const val coreKtx = "androidx.appcompat:appcompat:${Versions.core}"
-
+    const val coreKtx = "androidx.core:core-ktx:${Versions.core}"
+    const val jetbrainsKt = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.jetbrains}"
+    const val appCompatKt = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val androidMaterial = "com.google.android.material:material:${Versions.material}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraint}"
+    const val retrofitKt = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val coroutinesKt = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+    const val roomDataBase = "androidx.room:room-runtime:${Versions.room}"
+    const val daggerHilt = "com.google.dagger:hilt-android:${Versions.hilt}"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,5 +3,12 @@ object Versions {
     const val lifecycle = "2.2.0"
     const val arch = "2.1.0"
     const val core = "1.3.2"
-
+    const val jetbrains  = "1.4.10"
+    const val appcompat = "1.2.0"
+    const val material = "1.2.1"
+    const val constraint = "2.0.4"
+    const val retrofit = "2.9.0"
+    const val coroutines = "1.3.9"
+    const val room = "2.2.6"
+    const val hilt = "2.28-alpha"
 }
