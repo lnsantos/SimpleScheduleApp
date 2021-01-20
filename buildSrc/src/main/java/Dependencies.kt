3faa47814a1f3be9420b92a0916b5effe01081c1
lnsantos <lucas.nepomuceno1999@gmail.com>
2021-01-20 14:47:26 -0300

    creating basic config of buildSrc...

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,7 @@
+object Dependencies {
+
+    const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+    const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
+    const val coreKtx = "androidx.appcompat:appcompat:${Versions.core}"
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,7 @@
+object Versions {
+
+    const val lifecycle = "2.2.0"
+    const val arch = "2.1.0"
+    const val core = "1.3.2"
+
+}
